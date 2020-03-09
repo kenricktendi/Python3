b645b031b03b3060918392cc50da504db0a43be7
kenricktendi <56884321+kenricktendi@users.noreply.github.com>
2020-03-09 03:51:05 -0400

    Update stoplight.py

--- stoplight.py
+++ stoplight.py
@@ -14,18 +14,18 @@ light_radius = 35
 yellow light = canvas_height/2
 dist_between_lights = 50
 
-screen = Canvas(root, width= canvas_width, height=canvas_height, bg= "white")
-screen.pack()
+
 
 def draw_circle(x1, y1, x2, y2, color):
     screen.create_oval(x1, y1, x2, y2, color)
 
+screen = Canvas(root, width= canvas_width, height=canvas_height, bg= "white")
+screen.pack()
 
-screen.create_oval(170, 30, 220, 80, fill="red")
-screen.create_rectangle(160, 10, 230, 280, fill ="light gray")
-screen.create_oval(170, 120, 220, 170, fill="yellow")
-screen.create_oval(170, 210 , 220, 260 , fill="green")
-draw_circle()
+screen.create_rectangle(canvas_width/2 - 60, canvas_height/2 - 175, canvas_width/2 + 60, canvas_height/2 + 175, fill='#808080')
+
+
+draw_circle(canvas_width/2, canvas_height - circle_radius, canvas_width/2, canvas_height + circle_radius, fill= "red")
 
 
 
