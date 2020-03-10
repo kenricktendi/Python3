f476a5d73d340e5ece29224322960645a08ed925
kenricktendi <56884321+kenricktendi@users.noreply.github.com>
2020-03-10 09:54:58 -0400

    3/10

---
+++ growingcircles.py
@@ -0,0 +1,15 @@
+#Kenrick Tendi 
+#Exercise 2
+#3/9/20
+
+from tkinter import *
+root= Tk()
+
+canvas_height = 400
+canvas_width = 400
+radius = 40
+
+screen = Canvas(root, height = canvas_height, width = canvas_width)
+screen.pack()
+ 
+mainloop()
--- stoplight.py
+++ stoplight.py
@@ -6,27 +6,33 @@ from tkinter import *
 
 root = Tk()
 
-
-
 canvas_width = 300
 canvas_height = 400
 light_radius = 35
-yellow light = canvas_height/2
+stoplight_width = 120
+stoplight_height = 350
 dist_between_lights = 50
 
+yellow_center = canvas_height/2
+centerX = canvas_width/2
+red_center = yellow_center - dist_between_lights - (light_radius * 2)
+green_center = yellow_center + dist_between_lights + (light_radius * 2)
 
 
 def draw_circle(x1, y1, x2, y2, color):
-    screen.create_oval(x1, y1, x2, y2, color)
+        screen.create_oval(x1, y1, x2, y2, fill=color)
 
-screen = Canvas(root, width= canvas_width, height=canvas_height, bg= "white")
+# Create canvas
+screen = Canvas(root, width=canvas_width, height=canvas_height, bg="white")
 screen.pack()
+
+# Write your program here!
 
 screen.create_rectangle(canvas_width/2 - 60, canvas_height/2 - 175, canvas_width/2 + 60, canvas_height/2 + 175, fill='#808080')
 
-
-draw_circle(canvas_width/2, canvas_height - circle_radius, canvas_width/2, canvas_height + circle_radius, fill= "red")
-
-
-
+canvas_width/2 -35, canvas_height/2 - 35, canvas_width/2 + 35, canvas_height/2 + 35
+draw_circle(centerX - light_radius, red_center - light_radius, centerX + light_radius, red_center + light_radius, 'red')
+draw_circle(centerX - light_radius, yellow_center - light_radius, centerX + light_radius, yellow_center + light_radius, 'yellow')
+draw_circle(centerX - light_radius, green_center - light_radius, centerX + light_radius, green_center + light_radius, 'green')
+# Add shapes to canvas
 mainloop()
