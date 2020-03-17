46cd294f1216c5fab55e2afc69da4251197d6dae
kenricktendi <56884321+kenricktendi@users.noreply.github.com>
2020-03-17 10:46:13 -0400

    3/17/20

--- exercise3unit2.py
+++ exercise3unit2.py
@@ -4,3 +4,28 @@
 
 from tkinter import * 
 from turtle import * 
+
+num1 = int(input("what is the first number"))
+num2 = int(input("What is the second number"))
+
+
+
+def multiply(num1, num2):
+    total1 = num1 * num2 
+    print (str(total1))
+def add(num1, num2):
+    total2 = num1 + num2 
+    print (str(total2))
+def subtract(num1, num2):
+    total3 = num1 - num2
+    print(str(total3))
+opt = input("What do you want to do? (subtract, add, multiply)")
+
+if (opt=="add"):
+    add(num1, num2)
+elif (opt=="multiply"):
+    multiply(num1, num2)
+elif (opt=="subtract"):
+    subtract(num1, num2)
+
+
